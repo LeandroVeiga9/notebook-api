e05f0a25549dfd163513f18d32771272f4b87f39
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2022-04-06 17:52:08 -0300

    i18n

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,39 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind #, optional: true
 
-  def author
-    "Leandro Veiga"
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+
+  def to_br
+    {
+      name: self.name,
+      email: self.email,
+      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    }
   end
 
-  def as_json(options={})
-    super(methods: :author, root: true) 
-  end
+  # def author
+  #   "Jackson Pires"
+  # end
+
+  # def kind_description
+  #   self.kind.description
+  # end
+
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_desription, :author],
+  #     include: { kind: { only: :description }}
+  #   )
+  # end
+
+  # def hello
+  #   I18n.t('hello')
+  # end
+
+  # def i18n
+  #   I18n.default_locale
+  # end
 end
