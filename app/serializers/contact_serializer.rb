f1f32b5a8b536a13824d645bd14a1ea239379b88
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2022-04-07 15:40:44 -0300

    serializer

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related) { kind_url(object.kind.id) }
+  end
+
   has_many :phones
   has_one :address
+
+  # link(:self) { contact_url(object.id) }
+  # link(:kind) { kind_url(object.kind.id) }
+
+  meta do
+    { author: "Jackson Pires" }
+  end
 
   def attributes(*args)
     h = super(*args)
