4cf1877c9df59114ad5908fa44097e77df41cea3
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2022-04-07 17:36:13 -0300

    ams

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,9 @@ class Contact < ApplicationRecord
   has_many :phones
   has_one :address
 
+  before_destroy :destroy_phones
+  before_destroy :destroy_addresses
+
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
 
@@ -12,39 +15,14 @@ class Contact < ApplicationRecord
     h
   end
 
-  # def birthdate_br
-  #   I18n.l(self.birthdate) unless self.birthdate.blank?
-  # end
+  private
+   
+    def destroy_phones
+      self.phones.destroy_all
+    end
+  
+    def destroy_addresses
+      self.address.destroy
+    end
 
-  # def to_br
-  #   {
-  #     name: self.name,
-  #     email: self.email,
-  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-  #   }
-  # end
-
-  # def author
-  #   "Jackson Pires"
-  # end
-
-  # def kind_description
-  #   self.kind.description
-  # end
-
-  # def as_json(options={})
-  #   super(
-  #     root: true,
-  #     methods: [:kind_desription, :author],
-  #     include: { kind: { only: :description }}
-  #   )
-  # end
-
-  # def hello
-  #   I18n.t('hello')
-  # end
-
-  # def i18n
-  #   I18n.default_locale
-  # end
 end
